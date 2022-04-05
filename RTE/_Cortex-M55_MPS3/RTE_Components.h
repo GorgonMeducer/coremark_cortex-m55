3e9087be14206619d39fbc61a9a6bf89cf215ec3
Gabriel Wang <gabriel.wang@arm.com>
2022-04-05 17:17:04 +0100

    empty project with printf

---
+++ RTE/_Cortex-M55_MPS3/RTE_Components.h
@@ -0,0 +1,31 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'lv_template' 
+ * Target:  'Cortex-M55_MPS3' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "SSE300MPS3.h"
+
+/* ARM::CMSIS Driver:USART:1.0.0 */
+#define RTE_Drivers_USART
+/* GorgonMeducer.Performance Counter::Utilities:perf_counter:Core:Library:1.9.3 */
+#define __PERF_COUNTER__
+/* Keil.ARM Compiler::Compiler:Event Recorder:DAP:1.4.0 */
+#define RTE_Compiler_EventRecorder
+          #define RTE_Compiler_EventRecorder_DAP
+/* Keil.ARM Compiler::Compiler:I/O:STDOUT:EVR:1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_EVR      /* Compiler I/O: STDOUT EVR */
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ main.c
@@ -0,0 +1,58 @@
+/****************************************************************************
+*  Copyright 2021 Gorgon Meducer (Email:embedded_zhuoran@hotmail.com)       *
+*                                                                           *
+*  Licensed under the Apache License, Version 2.0 (the "License");          *
+*  you may not use this file except in compliance with the License.         *
+*  You may obtain a copy of the License at                                  *
+*                                                                           *
+*     http://www.apache.org/licenses/LICENSE-2.0                            *
+*                                                                           *
+*  Unless required by applicable law or agreed to in writing, software      *
+*  distributed under the License is distributed on an "AS IS" BASIS,        *
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+*  See the License for the specific language governing permissions and      *
+*  limitations under the License.                                           *
+*                                                                           *
+****************************************************************************/
+
+/*============================ INCLUDES ======================================*/
+#include "app_cfg.h"
+#include "platform.h"
+#include "cmsis_compiler.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdarg.h>
+
+#if defined(__clang__)
+#   pragma clang diagnostic ignored "-Wcompound-token-split-by-macro"
+#endif
+
+/*============================ MACROS ========================================*/
+/*============================ MACROFIED FUNCTIONS ===========================*/
+/*============================ TYPES =========================================*/
+/*============================ GLOBAL VARIABLES ==============================*/
+/*============================ LOCAL VARIABLES ===============================*/
+/*============================ PROTOTYPES ====================================*/
+/*============================ IMPLEMENTATION ================================*/
+
+
+void SysTick_Handler(void)
+{
+
+}
+
+
+int main(void)
+{
+    __cycleof__() {
+        printf("Hello World! \r\n");
+    }
+    
+    while(1) {
+
+    }
+    
+}
